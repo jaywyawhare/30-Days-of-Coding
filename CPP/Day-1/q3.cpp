713e496422d34ba766333e04f97f63f2e807ddbf
Arinjay <2005441@kiit.ac.in>
2023-01-04 23:43:05 +0530

    Day 1

---
+++ CPP/Day-1/q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    cout << "Enter 2 numbers ";
+    cin >> num1 >> num2;
+    cout << "The sum of both numbers is : " << num1 + num2;
+}
